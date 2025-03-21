cef13af5c4821aa45442f879162958b177730bbb
Amirali Mansouri <amiralimansouri98@outlook.com>
2025-03-21 22:52:39 +0330

    feat: Layout, Header and Footer

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,21 @@
+import styles from "./Layout.module.css";
+
+export default function Layout() {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>Crypto App</h1>
+        <p>
+          <span>Amirali</span> | React.js Tutorial
+        </p>
+      </header>
+      <main className={styles.content}></main>
+      <footer className={styles.footer}>
+        <p>
+          Develop and Design by Amirali Mansouri | Please Check my{" "}
+          <a href="https://github.com/amirali-98">Github</a>
+        </p>
+      </footer>
+    </>
+  );
+}
